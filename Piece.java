28dcea453f83c26f3ea59a38b216fc77b5486717
Hardy-orchis <hardy.rakotonarivo@orchisintl.com>
2025-12-23 22:47:08 +0100

    refqctorisqtion

--- Piece.java
+++ Piece.java
@@ -42,250 +42,144 @@ public class Piece extends JPanel{
     }
 
 
-    public ArrayList<Position> listeDesMouvementPossible(Case[][] listeDesCase){
-        List<Position> positions = new ArrayList<>();
-        if(!this.premierCout){
-            if("Pion".equals(this.nom) && "noir".equals(this.couleur)){
-                if(listeDesCase[this.position.x - 1][this.position.y].piece==null){
-                    ajouterSiValide(positions, this.position.x - 1, this.position.y);
+    private void ajouterMouvementLigne(Case[][] listeDesCase, List<Position> positions, int dx, int dy) {
+        int x = this.position.x;
+        int y = this.position.y;
+
+        while (true) {
+            x += dx;
+            y += dy;
+
+            if (x < 0 || x >= 8 || y < 0 || y >= 8) break;
+            if (listeDesCase[x][y].piece == null) {
+                ajouterSiValide(positions, x, y);
+            } else {
+                if (!listeDesCase[x][y].piece.couleur.equals(this.couleur)) {
+                    ajouterSiValide(positions, x, y);
                 }
-                return new ArrayList<>(positions);
-            }
-            
-            if("Pion".equals(this.nom) && "blanc".equals(this.couleur)){
-                if(listeDesCase[this.position.x + 1][this.position.y].piece==null){
-                    ajouterSiValide(positions, this.position.x + 1, this.position.y);
-                }
-                return new ArrayList<>(positions);
-            }
-        }else {
-             if("Pion".equals(this.nom) && "noir".equals(this.couleur)){
-                if(listeDesCase[this.position.x - 1][this.position.y].piece==null){
-                    ajouterSiValide(positions, this.position.x - 1, this.position.y);
-                    ajouterSiValide(positions, this.position.x - 2, this.position.y);
-                }
-                return new ArrayList<>(positions);
-            }
-            
-            if("Pion".equals(this.nom) && "blanc".equals(this.couleur)){
-                if(listeDesCase[this.position.x + 1][this.position.y].piece==null){
-                    ajouterSiValide(positions, this.position.x + 1, this.position.y);
-                    ajouterSiValide(positions, this.position.x + 2, this.position.y);
-                }
-                return new ArrayList<>(positions);
+                break;
             }
         }
-        
-        if("Cavalier".equals(this.nom)){
+    }
+
+    private void ajouterMouvementDiagonale(Case[][] listeDesCase, List<Position> positions, int dx, int dy) {
+        int x = this.position.x;
+        int y = this.position.y;
+
+        while (true) {
+            x += dx;
+            y += dy;
+
+            if (x < 0 || x >= 8 || y < 0 || y >= 8) break;
+            if (listeDesCase[x][y].piece == null) {
+                ajouterSiValide(positions, x, y);
+            } else {
+                if (!listeDesCase[x][y].piece.couleur.equals(this.couleur)) {
+                    ajouterSiValide(positions, x, y);
+                }
+                break;
+            }
+        }
+    }
+
+    public ArrayList<Position> listeDesMouvementPossible(Case[][] listeDesCase) {
+        List<Position> positions = new ArrayList<>();
+
+        if ("Pion".equals(this.nom)) {
+            int direction = "noir".equals(this.couleur) ? -1 : 1;
+            int startRow = this.position.x + direction;
+
+            if (!this.premierCout) {
+                if (listeDesCase[startRow][this.position.y].piece == null) {
+                    ajouterSiValide(positions, startRow, this.position.y);
+                }
+
+                if (this.position.y + 1 < 8 && listeDesCase[startRow][this.position.y + 1].piece != null) {
+                    ajouterSiValide(positions, startRow, this.position.y + 1);
+                }
+                if (this.position.y - 1 >= 0 && listeDesCase[startRow][this.position.y - 1].piece != null) {
+                    ajouterSiValide(positions, startRow, this.position.y - 1);
+                }
+            } else {
+                if (listeDesCase[startRow][this.position.y].piece == null) {
+                    ajouterSiValide(positions, startRow, this.position.y);
+                    int doubleMoveRow = this.position.x + 2 * direction;
+                    if (listeDesCase[doubleMoveRow][this.position.y].piece == null) {
+                        ajouterSiValide(positions, doubleMoveRow, this.position.y);
+                    }
+                }
+
+                if (this.position.y + 1 < 8 && listeDesCase[startRow][this.position.y + 1].piece != null) {
+                    ajouterSiValide(positions, startRow, this.position.y + 1);
+                }
+                if (this.position.y - 1 >= 0 && listeDesCase[startRow][this.position.y - 1].piece != null) {
+                    ajouterSiValide(positions, startRow, this.position.y - 1);
+                }
+            }
+        }
+
+        if ("Cavalier".equals(this.nom)) {
             int[][] mouvements = {
                 {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
                 {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
             };
-            
-            for(int[] mouv : mouvements){
+
+            for (int[] mouv : mouvements) {
                 int newX = this.position.x + mouv[0];
                 int newY = this.position.y + mouv[1];
-                
-                if(newX >= 0 && newX < 8 && newY >= 0 && newY < 8){
-                    if(listeDesCase[newX][newY].piece == null || 
-                    !listeDesCase[newX][newY].piece.couleur.equals(this.couleur)){
+
+                if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+                    if (listeDesCase[newX][newY].piece == null || 
+                    !listeDesCase[newX][newY].piece.couleur.equals(this.couleur)) {
                         ajouterSiValide(positions, newX, newY);
                     }
                 }
             }
-            return new ArrayList<>(positions);
         }
-        
-        if("Roi".equals(this.nom)){
-            for(int dx = -1; dx <= 1; dx++){
-                for(int dy = -1; dy <= 1; dy++){
-                    if(dx != 0 || dy != 0){
+
+        if ("Roi".equals(this.nom)) {
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    if (dx != 0 || dy != 0) {
                         int newX = this.position.x + dx;
                         int newY = this.position.y + dy;
-                        if(newX >= 0 && newX < 8 && newY >= 0 && newY < 8){
-                            if(listeDesCase[newX][newY].piece == null || 
-                            !listeDesCase[newX][newY].piece.couleur.equals(this.couleur)){
+                        if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+                            if (listeDesCase[newX][newY].piece == null || 
+                            !listeDesCase[newX][newY].piece.couleur.equals(this.couleur)) {
                                 ajouterSiValide(positions, newX, newY);
                             }
                         }
                     }
                 }
             }
-            return new ArrayList<>(positions);
         }
-        
-        if("Dame".equals(this.nom)){
-            for(int i = this.position.x + 1; i < 8; i++){
-                if(listeDesCase[i][this.position.y].piece == null){
-                    ajouterSiValide(positions, i, this.position.y);
-                } else {
-                    if(!listeDesCase[i][this.position.y].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, i, this.position.y);
-                    }
-                    break; 
-                }
-            }
-            for(int i = this.position.x - 1; i >= 0; i--){
-                if(listeDesCase[i][this.position.y].piece == null){
-                    ajouterSiValide(positions, i, this.position.y);
-                } else {
-                    if(!listeDesCase[i][this.position.y].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, i, this.position.y);
-                    }
-                    break;
-                }
-            }
-            for(int i = this.position.y + 1; i < 8; i++){
-                if(listeDesCase[this.position.x][i].piece == null){
-                    ajouterSiValide(positions, this.position.x, i);
-                } else {
-                    if(!listeDesCase[this.position.x][i].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x, i);
-                    }
-                    break;
-                }
-            }
-            for(int i = this.position.y - 1; i >= 0; i--){
-                if(listeDesCase[this.position.x][i].piece == null){
-                    ajouterSiValide(positions, this.position.x, i);
-                } else {
-                    if(!listeDesCase[this.position.x][i].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x, i);
-                    }
-                    break;
-                }
-            }
-            
-            for(int d = 1; this.position.x + d < 8 && this.position.y + d < 8; d++){
-                if(listeDesCase[this.position.x + d][this.position.y + d].piece == null){
-                    ajouterSiValide(positions, this.position.x + d, this.position.y + d);
-                } else {
-                    if(!listeDesCase[this.position.x + d][this.position.y + d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x + d, this.position.y + d);
-                    }
-                    break;
-                }
-            }
-            for(int d = 1; this.position.x + d < 8 && this.position.y - d >= 0; d++){
-                if(listeDesCase[this.position.x + d][this.position.y - d].piece == null){
-                    ajouterSiValide(positions, this.position.x + d, this.position.y - d);
-                } else {
-                    if(!listeDesCase[this.position.x + d][this.position.y - d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x + d, this.position.y - d);
-                    }
-                    break;
-                }
-            }
-            for(int d = 1; this.position.x - d >= 0 && this.position.y + d < 8; d++){
-                if(listeDesCase[this.position.x - d][this.position.y + d].piece == null){
-                    ajouterSiValide(positions, this.position.x - d, this.position.y + d);
-                } else {
-                    if(!listeDesCase[this.position.x - d][this.position.y + d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x - d, this.position.y + d);
-                    }
-                    break;
-                }
-            }
-            for(int d = 1; this.position.x - d >= 0 && this.position.y - d >= 0; d++){
-                if(listeDesCase[this.position.x - d][this.position.y - d].piece == null){
-                    ajouterSiValide(positions, this.position.x - d, this.position.y - d);
-                } else {
-                    if(!listeDesCase[this.position.x - d][this.position.y - d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x - d, this.position.y - d);
-                    }
-                    break;
-                }
-            }
-            return new ArrayList<>(positions);
+
+        if ("Dame".equals(this.nom)) {
+            ajouterMouvementLigne(listeDesCase, positions, 1, 0);  
+            ajouterMouvementLigne(listeDesCase, positions, -1, 0); 
+            ajouterMouvementLigne(listeDesCase, positions, 0, 1);  
+            ajouterMouvementLigne(listeDesCase, positions, 0, -1); 
+            ajouterMouvementDiagonale(listeDesCase, positions, 1, 1);  
+            ajouterMouvementDiagonale(listeDesCase, positions, -1, 1); 
+            ajouterMouvementDiagonale(listeDesCase, positions, 1, -1); 
+            ajouterMouvementDiagonale(listeDesCase, positions, -1, -1); 
         }
-        if("Fou".equals(this.nom)){
-            for(int d = 1; this.position.x + d < 8 && this.position.y + d < 8; d++){
-                if(listeDesCase[this.position.x + d][this.position.y + d].piece == null){
-                    ajouterSiValide(positions, this.position.x + d, this.position.y + d);
-                } else {
-                    if(!listeDesCase[this.position.x + d][this.position.y + d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x + d, this.position.y + d);
-                    }
-                    break;
-                }
-            }
-            for(int d = 1; this.position.x + d < 8 && this.position.y - d >= 0; d++){
-                if(listeDesCase[this.position.x + d][this.position.y - d].piece == null){
-                    ajouterSiValide(positions, this.position.x + d, this.position.y - d);
-                } else {
-                    if(!listeDesCase[this.position.x + d][this.position.y - d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x + d, this.position.y - d);
-                    }
-                    break;
-                }
-            }
-            for(int d = 1; this.position.x - d >= 0 && this.position.y + d < 8; d++){
-                if(listeDesCase[this.position.x - d][this.position.y + d].piece == null){
-                    ajouterSiValide(positions, this.position.x - d, this.position.y + d);
-                } else {
-                    if(!listeDesCase[this.position.x - d][this.position.y + d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x - d, this.position.y + d);
-                    }
-                    break;
-                }
-            }
-            for(int d = 1; this.position.x - d >= 0 && this.position.y - d >= 0; d++){
-                if(listeDesCase[this.position.x - d][this.position.y - d].piece == null){
-                    ajouterSiValide(positions, this.position.x - d, this.position.y - d);
-                } else {
-                    if(!listeDesCase[this.position.x - d][this.position.y - d].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x - d, this.position.y - d);
-                    }
-                    break;
-                }
-            }
-            return new ArrayList<>(positions);
+
+        if ("Fou".equals(this.nom)) {
+            ajouterMouvementDiagonale(listeDesCase, positions, 1, 1); 
+            ajouterMouvementDiagonale(listeDesCase, positions, -1, 1); 
+            ajouterMouvementDiagonale(listeDesCase, positions, 1, -1); 
+            ajouterMouvementDiagonale(listeDesCase, positions, -1, -1); 
         }
-        
-        if("Tour".equals(this.nom)){
-            for(int i = this.position.x + 1; i < 8; i++){
-                if(listeDesCase[i][this.position.y].piece == null){
-                    ajouterSiValide(positions, i, this.position.y);
-                } else {
-                    if(!listeDesCase[i][this.position.y].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, i, this.position.y);
-                    }
-                    break;
-                }
-            }
-            for(int i = this.position.x - 1; i >= 0; i--){
-                if(listeDesCase[i][this.position.y].piece == null){
-                    ajouterSiValide(positions, i, this.position.y);
-                } else {
-                    if(!listeDesCase[i][this.position.y].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, i, this.position.y);
-                    }
-                    break;
-                }
-            }
-            for(int i = this.position.y + 1; i < 8; i++){
-                if(listeDesCase[this.position.x][i].piece == null){
-                    ajouterSiValide(positions, this.position.x, i);
-                } else {
-                    if(!listeDesCase[this.position.x][i].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x, i);
-                    }
-                    break;
-                }
-            }
-            for(int i = this.position.y - 1; i >= 0; i--){
-                if(listeDesCase[this.position.x][i].piece == null){
-                    ajouterSiValide(positions, this.position.x, i);
-                } else {
-                    if(!listeDesCase[this.position.x][i].piece.couleur.equals(this.couleur)){
-                        ajouterSiValide(positions, this.position.x, i);
-                    }
-                    break;
-                }
-            }
-            return new ArrayList<>(positions);
+
+        if ("Tour".equals(this.nom)) {
+            ajouterMouvementLigne(listeDesCase, positions, 1, 0);  
+            ajouterMouvementLigne(listeDesCase, positions, -1, 0); 
+            ajouterMouvementLigne(listeDesCase, positions, 0, 1); 
+            ajouterMouvementLigne(listeDesCase, positions, 0, -1);
         }
-        
-        return new ArrayList<>();
+
+        return new ArrayList<>(positions);
     }
+
 }
